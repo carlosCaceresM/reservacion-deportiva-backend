d1dc8af42752cfe7623fcf4a4e7385a93cc648dc
Carlos H Caceres M <carlos.caceres@ceiba.com.co>
2022-04-28 07:53:24 -0500

    Se elimina import no utilizado

--- comun/comun-dominio/src/main/java/com/ceiba/dominio/ValidadorArgumento.java
+++ comun/comun-dominio/src/main/java/com/ceiba/dominio/ValidadorArgumento.java
@@ -4,7 +4,6 @@ import com.ceiba.dominio.excepcion.ExcepcionLongitudValor;
 import com.ceiba.dominio.excepcion.ExcepcionValorInvalido;
 import com.ceiba.dominio.excepcion.ExcepcionValorObligatorio;
 
-import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
